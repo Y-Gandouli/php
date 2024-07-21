e0c9f31cdfc9cfdb8b94b545bae115ced3e1ddfc
Y-Gandouli <elgandouli.youssef@gmail.com>
2024-07-21 22:03:16 +0100

    msg

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
 
-    echo "Hello World!";
+echo "Hello World!";
 
-    // Enable us to use Headers
-    ob_start();
+// Enable us to use Headers
+ob_start();
 
-    // Set sessions
-    if(!isset($_SESSION)) {
-        session_start();
-    }
+// Set sessions
+if(!isset($_SESSION)) {
+    session_start();
+}
 
-    $hostname = "mydatabase.mysql.database.azure.com";
-    $username = "admin01";
-    $password = "wqL!Qmaj57k8Sb#";
-    $dbname = "mytable";
-    
-    $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.") ;
+$hostname = "mydatabase.mysql.database.azure.com";
+$username = "admin01";
+$password = "wqL!Qmaj57k8Sb#";
+$dbname = "mytable";
+
+$connection = mysqli_connect($hostname, $username, $password, $dbname);
+
+if ($connection) {
+    echo "Database connection established successfully.";
+} else {
+    die("Database connection not established.");
+}
