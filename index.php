73d5c5d7f24409a2e3dfc60d054976fd51af8fa2
Y-Gandouli <elgandouli.youssef@gmail.com>
2024-07-22 08:56:46 +0100

    code 3

--- index.php
+++ index.php
@@ -2,26 +2,22 @@
 
 echo "Hello World!<br>";
 
-// Enable us to use Headers
-ob_start();
-
 // Set sessions
 if (!isset($_SESSION)) {
     session_start();
 }
 
 $hostname = "mydatabase.mysql.database.azure.com";
-$username = "admin01";
+$username = "admin01@mydatabase";  // Ensure the username includes the server name
 $password = "wqL!Qmaj57k8Sb#";
 $dbname = "mytable";
 
+// Establish a connection to the database
 $connection = mysqli_connect($hostname, $username, $password, $dbname);
 
+// Check connection
 if ($connection) {
     echo "Database connection established successfully.";
 } else {
     echo "Database connection not established. Error: " . mysqli_connect_error();
 }
-
-// Flush the output buffer
-ob_end_flush();
